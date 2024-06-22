74d22f8ef9b1618b747503916266616f47951deb
Bryce Nicholson <bhnichol22@gmail.com>
2024-06-21 20:47:30 -0400

    Initial Commit

---
+++ BudgetBuild/BudgetBuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetBuildApp.swift
+//  BudgetBuild
+//
+//  Created by Bryce Nicholson on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct BudgetBuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
